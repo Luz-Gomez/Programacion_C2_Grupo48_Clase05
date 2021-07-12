0613fdaa2c47c5f49653e5a3257c41ae3ee5bbef
Luz Gomez <luz.torrente2020@gmail.com>
2021-07-12 17:20:33 -0500

    Iniciando la creacion de clases

---
+++ src/main/java/co/edu/utp/misiontic2022/c2/Arquitectos.java
@@ -0,0 +1,10 @@
+package co.edu.utp.misiontic2022.c2;
+/**
+ * Un plano requiere uno o varios Arquitectos, se desea tener un codigo y un
+ * nombre para identificarlos.
+ */
+public class Arquitectos {
+    Integer codigo;
+    String nombre;
+    
+}
